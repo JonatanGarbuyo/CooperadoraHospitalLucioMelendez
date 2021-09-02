45d8b8befd5c85ae9e8bbf9a885e986052a00c0d
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-09-02 09:23:14 -0300

    wraped 'a' tags with <Link> components

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,7 @@
-import styles from "./index.module.css";
 import {useState} from "react";
+import Link from 'next/link'
+
+import styles from "./index.module.css";
 
 
 export default function Header() {
@@ -19,26 +21,26 @@ export default function Header() {
 
   const listItems = navItems.map((item, index) => (
     <li key={`item-${index}`} className={styles.menu_item}>
-      <a className={styles.menu_item_a} 
-        href={ item==="Home"? "/" : `/${item}`} 
-      > 
-        {item}
-      </a>
+      <Link href={ item==="Home"? "/" : `/${item}`} >
+        <a className={styles.menu_item_a}> 
+          {item}
+        </a>
+      </Link>
     </li>
   ))
-
 
   return (
     <header className={styles.container}>
       <nav className={styles.nav}>
-
         <div className={styles.nav_mobile}>
-          <a href="/" className={styles.logo_a}>
-            <img className={styles.logo_img}
-              src="/images/coop_logo_2.png"
-              alt="Asociacion Cooperadora Lucio Melendez logo"
-            />
-          </a>
+          <Link href="/">
+            <a className={styles.logo_a}>
+              <img className={styles.logo_img}
+                src="/images/coop_logo_2.png"
+                alt="Asociacion Cooperadora Lucio Melendez logo"
+              />
+            </a>
+          </Link>
           <div className={styles.nav_toggle} onClick={navToggle}>
             <span className={styles.bar1}/>
             <span className={styles.bar2}/>
@@ -65,18 +67,18 @@ export default function Header() {
           >✕</div>
 
           <div className={styles.logo_wraper}>
-            <a href="/" className={styles.logo_a}>
-              <img className={styles.logo_img}
-                src="/images/coop_logo_2.png"
-                alt="Asociacion Cooperadora Lucio Melendez logo"
-              />
-            </a>
+            <Link href="/">
+              <a href="/" className={styles.logo_a}>
+                <img className={styles.logo_img}
+                  src="/images/coop_logo_2.png"
+                  alt="Asociacion Cooperadora Lucio Melendez logo"
+                />
+              </a>
+            </Link>
           </div>
             <ul id="main_menu" className={styles.main_menu}>
               {listItems}
             </ul>
-
-
         </div>
       </nav>
     </header>
