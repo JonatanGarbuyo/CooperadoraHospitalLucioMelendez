fbc0e07dec0a3df2b691717d0c5343d3b8722697
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2023-06-08 14:42:54 -0300

    Fix Link component - remove child <a>

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,31 +1,31 @@
-import {useState} from "react";
+import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import styles from "./index.module.css";
-
+import styles from './index.module.css'
 
 export default function Header() {
-  const [showNavMenu, setShowNavMenu] = useState(false);
+  const [showNavMenu, setShowNavMenu] = useState(false)
 
   const navToggle = () => {
     setShowNavMenu(!showNavMenu)
   }
-  
+
   const navItems = [
-    "Home",
-    "Nosotros",
-    // "Historia", 
-    "Donar", 
-    "Contacto"
+    'Home',
+    'Nosotros',
+    // "Historia",
+    'Donar',
+    'Contacto',
   ]
 
   const listItems = navItems.map((item, index) => (
     <li key={`item-${index}`} className={styles.menu_item}>
-      <Link href={ item==="Home"? "/" : `/${item}`} >
-        <a className={styles.menu_item_a}> 
-          {item}
-        </a>
+      <Link
+        className={styles.menu_item_a}
+        href={item === 'Home' ? '/' : `/${item}`}
+      >
+        {item}
       </Link>
     </li>
   ))
@@ -34,56 +34,49 @@ export default function Header() {
     <header className={styles.container}>
       <nav className={styles.nav}>
         <div className={styles.nav_mobile}>
-          <Link href="/">
-            <a className={styles.logo_a}>
-              <Image className={styles.logo_img}
-                height="100"
-                width="400"
-                src="/images/coop_logo_2.png"
-                alt="Asociacion Cooperadora Lucio Melendez logo"
-              />
-            </a>
+          <Link className={styles.logo_a} href="/">
+            <Image
+              className={styles.logo_img}
+              height="100"
+              width="400"
+              src="/images/coop_logo_2.png"
+              alt="Asociacion Cooperadora Lucio Melendez logo"
+            />
           </Link>
           <div className={styles.nav_toggle} onClick={navToggle}>
-            <span className={styles.bar1}/>
-            <span className={styles.bar2}/>
-            <span className={styles.bar3}/>
+            <span className={styles.bar1} />
+            <span className={styles.bar2} />
+            <span className={styles.bar3} />
           </div>
         </div>
 
-        <div id="nav_menu_wrapper" 
-          className={styles.nav_menu_wrapper} 
-          style={
-            showNavMenu ?
-              {"left": 0} : 
-              null
-            }
+        <div
+          id="nav_menu_wrapper"
+          className={styles.nav_menu_wrapper}
+          style={showNavMenu ? { left: 0 } : null}
         >
           <div
             className={styles.nav_mobile_close_button}
-            style={
-              showNavMenu ? 
-                {"display": "block"} : 
-                {"display": "none"}
-              }
-            onClick={()=>setShowNavMenu(false)}
-          >✕</div>
+            style={showNavMenu ? { display: 'block' } : { display: 'none' }}
+            onClick={() => setShowNavMenu(false)}
+          >
+            ✕
+          </div>
 
           <div className={styles.logo_wraper}>
-            <Link href="/">
-              <a href="/" className={styles.logo_a}>
-                <img className={styles.logo_img}
-                  src="/images/coop_logo_2.png"
-                  alt="Asociacion Cooperadora Lucio Melendez logo"
-                />
-              </a>
+            <Link className={styles.logo_a} href="/">
+              <img
+                className={styles.logo_img}
+                src="/images/coop_logo_2.png"
+                alt="Asociacion Cooperadora Lucio Melendez logo"
+              />
             </Link>
           </div>
-            <ul id="main_menu" className={styles.main_menu}>
-              {listItems}
-            </ul>
+          <ul id="main_menu" className={styles.main_menu}>
+            {listItems}
+          </ul>
         </div>
       </nav>
     </header>
-  );
+  )
 }
