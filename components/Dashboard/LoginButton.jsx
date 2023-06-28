3c88d9a69c7b8a60e4ed93e9adf24c5e6817f839
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2023-06-28 13:49:04 -0300

    add login button

---
+++ components/Dashboard/LoginButton.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+export default function LoginButton() {
+	const { data: session } = useSession()
+	if (session) {
+		return (
+			<>
+				<p>Signed in as {session.user.name} </p>
+				<button onClick={() => signOut()}>Sign out</button>
+			</>
+		)
+	}
+	return (
+		<>
+			<p>Not signed in </p>
+			<button onClick={() => signIn()}>Sign in</button>
+		</>
+	)
+}
