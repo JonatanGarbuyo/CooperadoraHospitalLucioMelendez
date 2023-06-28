d9d3fb9317ec260928301df7920ddd26f3970966
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2023-06-28 13:44:08 -0300

    add user login

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,48 @@
+import NextAuth from 'next-auth'
+import bcrypt from 'bcrypt'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import prisma from '../../../../lib/prisma'
+
+const handler = NextAuth({
+	session: {
+		strategy: 'jwt',
+		maxAge: 30 * 24 * 60 * 60, // 30 days
+	},
+	providers: [
+		CredentialsProvider({
+			// The name to display on the sign in form (e.g. 'Sign in with...')
+			id: 'credentials',
+			type: 'credentials',
+			name: 'Email',
+			// You can pass any HTML attribute to the <input> tag through the object.
+			credentials: {
+				email: {
+					label: 'E-mail',
+					type: 'email',
+					placeholder: 'usuario@ejemplo.com',
+				},
+				password: { label: 'Contraseña', type: 'password' },
+			},
+
+			async authorize(credentials, req) {
+				const { email, password } = credentials
+				const user = await prisma.user.findUnique({ where: { email } })
+				const isValidPassword = await bcrypt.compare(password, user.password)
+				return isValidPassword ? user : null
+			},
+		}),
+	],
+
+	callbacks: {
+		async jwt({ token, user }) {
+			token.role = user?.role
+			return token
+		},
+	},
+
+	pages: {
+		error: '/dashboard/login',
+	},
+})
+
+export { handler as GET, handler as POST }
