a40cfa4dcc2a98d0cc10c9de87549f0eb25f64e5
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-10-12 15:05:19 -0300

    carpeta pages eliminada, tailwindcss instalado, estructura de carpetas actualizada

--- app/login/layout.jsx
+++ app/login/layout.jsx
@@ -1,4 +1,4 @@
-import '../../styles/login.css'
+import styles from './styles.module.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body style={{ minWidth: '375px' }} className={`${inter.className}`}>
+			<body
+				style={{ minWidth: '375px' }}
+				className={`${inter.className} ${styles.body}`}
+			>
 				{children}
 			</body>
 		</html>
