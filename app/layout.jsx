956a270bbb67611ba3c8e7bd99dffde984353c67
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-10-13 13:49:49 -0300

    carpeta pages eliminada, tailwindcss instalado, estructura de carpetas actualizada

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import './globals.css'
+
+export const metadata = {
+	title: 'Cooperadora Lucio Melendez',
+	description:
+		'Somos la Asociación Cooperadora del hospital Lucio Melendez. Esta pagina la creamos para acercarnos a la comunidad y mostrar lo que sus colaboraciones pueden lograr. Entre todos podemos ayudar a nuestro querido hospital. Por ello, tu aporte es muy importante para que podamos continuar con nuestra misión como Institución.',
+}
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	)
+}
