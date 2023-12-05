8356d99cd564e7f279417af2b10845f7ba97b69e
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-12-05 17:28:14 -0300

    adding hero, and header height variable

---
+++ components/Hero.jsx
@@ -0,0 +1,5 @@
+export default function Hero() {
+	return (
+		<section className="h-[calc(100vh-var(--header-height))] bg-primary"></section>
+	)
+}
--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -4,7 +4,7 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function MainHeader() {
 	return (
-		<header className="flex h-20 items-center justify-between bg-primary pl-4 pr-4">
+		<header className="flex h-[--header-height] items-center justify-between bg-primary pl-4 pr-4">
 			<Image src={logo} alt="logo de la cooperadora hospital lucio melendez" />
 			<div className="flex items-center gap-2">
 				<button className="rounded-10 bg-accent px-4 py-1 font-bold text-white">
