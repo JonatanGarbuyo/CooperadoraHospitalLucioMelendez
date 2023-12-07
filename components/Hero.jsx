4258e0279665102a52f855dba3499ae59bb6495b
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-12-07 10:39:22 -0300

    fixing Hero Image responsiveness

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import coopHeroImg from '@/public/images/coop_hero_hd.webp'
 
 export default function Hero() {
 	return (
-		<section className="flex h-[calc(100dvh-var(--header-height))] flex-col bg-primary pt-10">
+		<section className="flex h-[calc(100svh-var(--header-height))] flex-col bg-primary pt-10">
 			<div className="flex flex-col items-center justify-between gap-2 px-4">
 				<h1 className="text-center text-4xl font-bold tracking-tight text-white">
 					¡Ayudanos a Ayudar!
@@ -13,18 +13,19 @@ export default function Hero() {
 					Tu donación nos permite seguir ayudando a los que más lo necesitan.
 				</h3>
 			</div>
-			<button className="mb-2 mt-4 flex items-center justify-center gap-1 self-center rounded-10 border-2 border-accent px-4 py-2 font-bold text-white">
+			<button className="mb-2 mt-4 flex items-center justify-center gap-1 self-center rounded-10 bg-accent px-4 py-2 font-bold text-white">
 				<Image src={heartIcon} alt="Icono de manos formando un corazón" />
 				<span>DONAR</span>
 			</button>
-			<Image
-				src={coopHeroImg}
-				alt="Foto de Inauguración Sala de Oncología"
-				priority
-				width={2856}
-				height={1658}
-				className="h-full w-auto object-cover [mask-image:linear-gradient(transparent,rgba(0,0,0,1)_30%)]"
-			/>
+			<div className="relative flex-1">
+				<Image
+					src={coopHeroImg}
+					alt="Foto de Inauguración Sala de Oncología"
+					priority
+					fill
+					className="object-cover [mask-image:linear-gradient(transparent,rgba(0,0,0,1)_30%)]"
+				/>
+			</div>
 		</section>
 	)
 }
