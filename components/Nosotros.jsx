51fbdd791616ac88f0c66d282c09c02c2ac14321
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-12-25 17:30:40 -0300

    tablet version Nosotros section

--- components/Nosotros.jsx
+++ components/Nosotros.jsx
@@ -4,10 +4,10 @@ import miembrosCoop2 from '@/public/images/miembros_coop2.png'
 
 export default function Nosotros() {
 	return (
-		<section className="flex flex-col gap-6 bg-primary px-4 py-10 text-center text-white">
+		<section className="flex flex-col gap-6 bg-primary px-4 py-10 text-center text-white md:gap-8 md:px-10 md:py-16">
 			<h2>Nosotros</h2>
 			<div>
-				<div className="relative mx-auto mb-2 aspect-[3/2] w-[calc(100dvw-2rem)]">
+				<div className="relative mx-auto mb-2 aspect-[3/2] w-full md:mb-4">
 					<Image
 						src={miembrosCoop}
 						alt="Selfie grupal de los miembros de la cooperadora"
@@ -15,7 +15,7 @@ export default function Nosotros() {
 						className="rounded-10 object-cover"
 					/>
 				</div>
-				<p className="text-lg font-medium text-white">
+				<p className="text-balance text-lg font-medium text-white md:text-2xl">
 					La Asociación Cooperadora del Hospital Lucio Meléndez es una{' '}
 					<span className="font-bold">
 						Asociación Civil Sin Fines de Lucro.
@@ -23,7 +23,7 @@ export default function Nosotros() {
 				</p>
 			</div>
 			<div>
-				<div className="relative mx-auto mb-2 aspect-[3/2] w-[calc(100dvw-2rem)]">
+				<div className="relative mx-auto mb-2 aspect-[3/2] w-full md:mb-4">
 					<Image
 						src={miembrosCoop2}
 						alt="Selfie grupal de los miembros de la cooperadora"
@@ -31,7 +31,7 @@ export default function Nosotros() {
 						className="rounded-10 object-cover"
 					/>
 				</div>
-				<p className="text-lg font-medium text-white">
+				<p className="text-balance text-lg font-medium text-white md:text-2xl">
 					Nuestra Sede Central esta ubicada en el{' '}
 					<span className="font-bold">Hall Central del Hospital,</span> y la
 					atención es de{' '}
