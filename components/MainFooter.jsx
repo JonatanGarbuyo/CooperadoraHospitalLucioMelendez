70ab5974d12cc04aad5ab8379349f35b9285866a
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-12-26 15:19:37 -0300

    tablet version header & footer

--- components/MainFooter.jsx
+++ components/MainFooter.jsx
@@ -6,30 +6,28 @@ import { VscMail } from 'react-icons/vsc'
 
 export default function MainFooter() {
 	return (
-		<footer className="bg-primary px-4 py-10">
+		<footer className="bg-primary px-4 py-8 md:flex md:flex-row-reverse md:px-6 md:py-12">
 			<Image
 				src={logo}
 				alt="logo de la cooperadora hospital lucio melendez"
 				className="mx-auto w-full max-w-[300px] pb-6"
 			/>
 			<div>
-				<ul className="flex flex-col gap-3 [&>li]:flex [&>li]:items-center [&>li]:gap-[10px]">
+				<ul className="flex flex-col gap-3 [&>li]:flex [&>li]:items-center [&>li]:gap-[10px] [&_p]:text-sm [&_p]:text-white [&_p]:md:text-lg">
 					<li>
 						<SlLocationPin className="h-auto min-w-[1.75rem] fill-white" />
-						<p className="text-sm text-white">
+						<p className="text-balance">
 							Pres. Juan Domingo Perón 859, B1846 Adrogué, Provincia de Buenos
 							Aires
 						</p>
 					</li>
 					<li>
 						<BsTelephone className="h-auto w-7 fill-white" />
-						<p className="text-sm text-white">+54 11-4294-5555 int 320</p>
+						<p>+54 11-4294-5555 int 320</p>
 					</li>
 					<li>
 						<VscMail className="h-auto w-7 fill-white" />
-						<p className="text-sm text-white">
-							cooperadoraluciomelendez@gmail.com
-						</p>
+						<p>cooperadoraluciomelendez@gmail.com</p>
 					</li>
 				</ul>
 			</div>
--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,16 +1,26 @@
 import Image from 'next/image'
 import logo from '@/public/images/coop_new_logo.svg'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import heartIcon from '@/public/images/heart_icon.svg'
 
 export default function MainHeader() {
 	return (
-		<header className="fixed top-0 z-50 flex h-[--header-height] w-full items-center justify-between bg-primary pl-4 pr-4 shadow-sm">
-			<Image src={logo} alt="logo de la cooperadora hospital lucio melendez" />
-			<div className="flex items-center gap-2">
-				<button className="rounded-10 bg-accent px-4 py-1 font-bold text-white">
+		<header className="h- fixed top-0 z-50 flex h-[--header-height] w-full items-center justify-between bg-primary px-4 shadow-sm md:px-6 ">
+			<Image
+				src={logo}
+				alt="logo de la cooperadora hospital lucio melendez"
+				className="md:w-[249px]"
+			/>
+			<div className="flex items-center gap-2 md:gap-8">
+				<button className="flex items-center gap-2 rounded-10 bg-accent px-4 py-1 font-bold text-white md:px-8 md:py-3 md:text-xl">
+					<Image
+						src={heartIcon}
+						alt="Icono de manos formando un corazón"
+						className="max-md:hidden"
+					/>
 					DONAR
 				</button>
-				<GiHamburgerMenu className="h-auto w-6 fill-white" />
+				<GiHamburgerMenu className="h-auto w-6 fill-white md:w-9" />
 			</div>
 		</header>
 	)
