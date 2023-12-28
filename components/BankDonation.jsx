17601d84222c44544caacebd793072cb0d6f9014
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-12-28 15:01:53 -0300

    1280px bank donation section

--- components/BankDonation.jsx
+++ components/BankDonation.jsx
@@ -4,13 +4,13 @@ import logoMP from '@/public/images/logo_mercado_pago.svg'
 
 export default function BankDonation() {
 	return (
-		<section className="flex flex-col items-center gap-6 px-4 py-10 text-center md:gap-8 md:px-10 md:py-16">
-			<h2>
+		<section className="flex flex-col items-center gap-6 px-4 py-10 text-center md:gap-8 md:px-10 md:py-16 xl:text-left">
+			<h2 className="xl:self-start">
 				<span className="text-secondary">Donación Volunatria</span> para
 				colaborar con la compra de materiales
 			</h2>
-			<div className="rounded-10 px-2 py-4 shadow-[0px_0px_4px_0px_rgba(0,0,0,0.1)] md:px-4 md:py-7">
-				<div className="mb-6 md:mb-0 md:border-b-[1px] md:border-stone-400 md:pb-6">
+			<div className="relative rounded-10 px-2 py-4 shadow-[0px_1px_4px_1px_rgba(0,0,0,0.1)] md:px-4 md:py-7 xl:grid xl:grid-cols-2 xl:gap-8">
+				<div className="mb-6 md:mb-0 md:border-stone-400 md:pb-6 md:max-xl:border-b-[1px] xl:pb-0">
 					<p className="mb-4 text-balance text-xl font-medium text-black md:mb-5 md:text-3xl">
 						Podés hacerlo mediante una{' '}
 						<span className="text-secondary">transferencia o depósito</span> a:
@@ -45,8 +45,9 @@ export default function BankDonation() {
 						</li>
 					</ul>
 				</div>
+				<div className="absolute left-0 right-0 m-auto h-full w-px bg-slate-300 max-xl:hidden" />
 				<div>
-					<p className="mb-4 text-balance text-xl font-medium text-black md:mb-5 md:mt-6 md:text-3xl">
+					<p className="mb-4 text-pretty text-xl font-medium text-black md:mb-5 md:mt-6 md:text-3xl xl:mt-0">
 						También podés donar desde tu teléfono escaneando el{' '}
 						<span className="text-secondary">código QR</span>
 					</p>
