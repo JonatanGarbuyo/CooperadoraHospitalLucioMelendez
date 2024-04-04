389809f5bc33ae33ba0cfc9014aac362e3464f29
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2024-04-04 20:25:01 -0300

    fix: font import

--- app/(main site)/layout.jsx
+++ app/(main site)/layout.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line camelcase
-import { Baloo_2 } from 'next/font/google'
+import { Baloo_2 as Baloo2 } from 'next/font/google'
 import MainHeader from '@/components/MainHeader'
 import './main.css'
 import MainFooter from '@/components/MainFooter'
@@ -10,7 +9,7 @@ export const metadata = {
 		'Somos la Asociación Cooperadora del hospital Lucio Melendez. Esta pagina la creamos para acercarnos a la comunidad y mostrar lo que sus colaboraciones pueden lograr. Entre todos podemos ayudar a nuestro querido hospital. Por ello, tu aporte es muy importante para que podamos continuar con nuestra misión como Institución.',
 }
 
-const baloo = Baloo_2({ subsets: ['latin'], display: 'swap' })
+const baloo = Baloo2({ subsets: ['latin'], display: 'swap' })
 
 export default function RootLayout({ children }) {
 	return (
