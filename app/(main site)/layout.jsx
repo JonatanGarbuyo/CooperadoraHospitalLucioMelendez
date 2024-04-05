966c5c8c739353f5a8ec3ee216a57b52d5743ba8
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2024-04-04 21:37:42 -0300

    fix: spelling

--- app/(main site)/layout.jsx
+++ app/(main site)/layout.jsx
@@ -4,9 +4,9 @@ import './main.css'
 import MainFooter from '@/components/MainFooter'
 
 export const metadata = {
-	title: 'Cooperadora Lucio Melendez',
+	title: 'Cooperadora Lucio Meléndez',
 	description:
-		'Somos la Asociación Cooperadora del hospital Lucio Melendez. Esta pagina la creamos para acercarnos a la comunidad y mostrar lo que sus colaboraciones pueden lograr. Entre todos podemos ayudar a nuestro querido hospital. Por ello, tu aporte es muy importante para que podamos continuar con nuestra misión como Institución.',
+		'Somos la Asociación Cooperadora del hospital Lucio Meléndez. Esta página la creamos para acercarnos a la comunidad y mostrar lo que sus colaboraciones pueden lograr. Entre todos podemos ayudar a nuestro querido hospital. Por ello, tu aporte es muy importante para que podamos continuar con nuestra misión como Institución.',
 }
 
 const baloo = Baloo2({ subsets: ['latin'], display: 'swap' })
