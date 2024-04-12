f5425c3411ea430143b338cd71d0c8ceda7fadc3
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2024-04-11 22:23:48 -0300

    fix: add contact info link tags

--- components/MainFooter.jsx
+++ components/MainFooter.jsx
@@ -16,21 +16,27 @@ export default function MainFooter() {
 				className="mx-auto w-full max-w-[300px] pb-6 md:mx-0"
 			/>
 			<div>
-				<ul className="flex flex-col gap-3 [&>li]:flex [&>li]:items-center [&>li]:gap-[10px] [&_p]:text-sm [&_p]:text-white [&_p]:md:text-lg">
+				<ul className="flex flex-col gap-3 [&_a]:flex [&_a]:items-center 	[&_a]:gap-[10px] [&_p]:text-sm [&_p]:text-white [&_p]:md:text-lg">
 					<li>
-						<SlLocationPin className="h-auto min-w-[1.75rem] fill-white" />
-						<p className="text-balance">
-							Pres. Juan Domingo Perón 859, B1846 Adrogué, Provincia de Buenos
-							Aires
-						</p>
+						<a href="https://goo.gl/maps/ubQCGyqAjhgivnBA9" target="_blank">
+							<SlLocationPin className="h-auto min-w-[1.75rem] fill-white" />
+							<p className="text-balance">
+								Pres. Juan Domingo Perón 859, B1846 Adrogué, Provincia de Buenos
+								Aires
+							</p>
+						</a>
 					</li>
 					<li>
-						<BsTelephone className="h-auto w-7 fill-white" />
-						<p>+54 11-4294-5555 int 320</p>
+						<a href="tel:+541142945555">
+							<BsTelephone className="h-auto w-7 fill-white" />
+							<p>+54 11-4294-5555 int 320</p>
+						</a>
 					</li>
 					<li>
-						<VscMail className="h-auto w-7 fill-white" />
-						<p>cooperadoraluciomelendez@gmail.com</p>
+						<a href="mailto:cooperadoraluciomelendez@gmail.com?subject=Consulta%20desde%20la%20pagina">
+							<VscMail className="h-auto w-7 fill-white" />
+							<p>cooperadoraluciomelendez@gmail.com</p>
+						</a>
 					</li>
 				</ul>
 			</div>
