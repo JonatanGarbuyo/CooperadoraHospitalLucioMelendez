49ecbd1bed10d5a4e887f935b3ac9c9d6483c3ac
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2025-12-07 22:32:06 -0300

    Fix hydration errors in layouts and refactor font usage

--- app/(dashboard)/dashboard/layout.jsx
+++ app/(dashboard)/dashboard/layout.jsx
@@ -1,12 +1,8 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
 import styles from './layout.module.css'
 import './main.css'
 import LoginButton from '@/components/Dashboard/LoginButton'
 import Providers from '@/context/providers'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'Dashboard',
@@ -15,25 +11,23 @@ export const metadata = {
 
 export default function Layout({ children }) {
 	return (
-		<html lang="es">
-			<body className={`${inter.className} ${styles.body}`}>
-				<Providers>
-					<header className={styles.header}>
-						<nav className={styles.navigation}>
-							<Image
-								className=""
-								height="80"
-								width="300"
-								src="/images/coop_logo_2.png"
-								alt="Asociacion Cooperadora Lucio Melendez logo"
-							/>
+		<div className={styles.body}>
+			<Providers>
+				<header className={styles.header}>
+					<nav className={styles.navigation}>
+						<Image
+							className=""
+							height="80"
+							width="300"
+							src="/images/coop_logo_2.png"
+							alt="Asociacion Cooperadora Lucio Melendez logo"
+						/>
 
-							<LoginButton />
-						</nav>
-					</header>
-					<div className={styles.content}>{children}</div>
-				</Providers>
-			</body>
-		</html>
+						<LoginButton />
+					</nav>
+				</header>
+				<div className={styles.content}>{children}</div>
+			</Providers>
+		</div>
 	)
 }
--- app/(dashboard)/login/layout.jsx
+++ app/(dashboard)/login/layout.jsx
@@ -1,22 +1,14 @@
 import styles from './styles.module.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'Login',
 	description: 'Inicio de sesión',
 }
 
-export default function RootLayout({ children }) {
+export default function LoginLayout({ children }) {
 	return (
-		<html lang="en">
-			<body
-				style={{ minWidth: '375px' }}
-				className={`${inter.className} ${styles.body}`}
-			>
-				{children}
-			</body>
-		</html>
+		<div style={{ minWidth: '375px' }} className={styles.body}>
+			{children}
+		</div>
 	)
 }
